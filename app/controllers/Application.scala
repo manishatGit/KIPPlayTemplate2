77603de186b91d30650f281b46dbddad307689d9
Manish <reseamanish@gmail.com>
2015-02-17 19:51:56 +0530

    Play template first commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Home"))
+  }
+  def addemployee = Action {
+    Ok(views.html.addemployee("addemployee"))
+  }
+
+}
