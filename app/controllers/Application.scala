a0d8c39ec3e61b7e3ddb9419dc6ac1cbe9029b5e
Manish <reseamanish@gmail.com>
2015-02-24 12:47:37 +0530

    fourth commit with add, edit, delete

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,29 +1,100 @@
 package controllers
-import models._
+import org.joda.time.LocalDateTime
 import play.api._
-import play.api.mvc._
+import play.api.Play.current
 import play.api.data._
 import play.api.data.Forms._
 import play.api.db.slick._
-import play.api.Play.current
+import play.api.mvc._
 import views._
+import models.Knolder
+import models.knolTable
 
 object Application extends Controller {
 
+  /**
+   * ********************************************
+   * Home redirects to the default page showEmp *
+   * ********************************************
+   */
+  val Home = Redirect(routes.Application.index)
+
   /* Mapping knolder form
    */
-  
-  
-  def addemployee = Action {
-    Ok(views.html.addemployee("addemployee"))
-  }
-  def getKnolderController = Action {  
-    Ok(views.html.viewknolder("Render Emp"))    
-  }
-  def index: Action[AnyContent]= DBAction { implicit rs => 
+  val knolForm = Form(
+    mapping(
+      "id" -> ignored(Some(0): Option[Int]),
+      "name" -> text,
+      "email" -> nonEmptyText,
+      "mobile" -> nonEmptyText)(Knolder.apply)(Knolder.unapply))
+
+  /**
+   * *********************************************************************
+   * Handles the default action, renders list of employees from database *
+   * *********************************************************************
+   */
+
+  def index: Action[AnyContent] = DBAction { implicit rs =>
     Ok(views.html.index(knolTable.getKnolderList()))
-    
+
+  }
+  /**
+   * *****************************************
+   * Display the 'new Knolder form'.        *
+   * *****************************************
+   */
+  def create = Action {
+    Ok(html.createForm(knolForm))
+  }
+
+  /**
+   * *****************************************
+   * Display the 'edit Knolder Form'.        *
+   * *****************************************
+   * @param id of the Knolder to edit
+   */
+  def edit(knolId: Int) = DBAction { implicit request =>
+     knolTable.getKnolderById(knolId) match {
+      case knolder => Ok(html.editForm(knolId, knolForm.fill(knolder)))
+    }
+  }
+  /**
+   * *******************************************
+   * Handles the submit of 'new Knolder form' *
+   * *******************************************
+   */
+  def save = DBAction { implicit request =>
+    knolForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.createForm(formWithErrors)),
+      knolder => {
+        knolTable.insertKnolder(knolder)
+        val msg = "Company ${emp.name} has been created"
+        Home.flashing("success" -> msg)
+      })
+  }
+  /**
+   * ****************************************
+   * Handles the delete link of the record  *
+   * ****************************************
+   */
+  def deleteKnol(id: Int) = DBAction { implicit request =>
+    knolTable.deleteKnolder(id)
+    Home.flashing("success" -> "Emp has been deleted")
   }
   
-  
+  /**
+   * Handle the 'edit form' submission
+   *
+   * @param id Id of the Knolder to edit
+   */
+  def updateKnolder(knolderId: Int) = DBAction { implicit request =>
+    knolForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.editForm(knolderId, formWithErrors)),
+      knolder => {
+        val knolderToUpdate: Knolder = knolder.copy(Some(knolderId))
+        knolTable.updateKnolder(knolderToUpdate)
+        Home.flashing("success" -> s"Company ${knolder.name} has been updated")
+      })
+  }
 }
+
--- app/models/KnolderModel.scala
+++ app/models/KnolderModel.scala
@@ -1,5 +1,7 @@
 package models
+
 import play.api.db.slick.Config.driver.simple._
+import scala.collection.immutable.List
 
 /**
  * Model: Knolder contains case class Knolder and an Object with insert method
@@ -16,23 +18,56 @@ class KnolderTable(tag: Tag) extends Table[Knolder](tag, "Knolder") {
 object knolTable {
   val knolTableQuery = TableQuery[KnolderTable]
   def insert(knolder: Knolder)(implicit s: Session): Int = {
-    knolTable.insert(knolder)
+    knolTableQuery.insert(knolder)
   }
-  
-  /**************************************************************************************
+
+  /**
+   * ************************************************************************************
    * fetches the list of Knolder records from the database                              *
-   **************************************************************************************/
-  def getKnolderList()(implicit session:Session):List[Knolder]={
+   * ************************************************************************************
+   */
+  def getKnolderList()(implicit session: Session): List[Knolder] = {
     knolTableQuery.list
   }
-  /**************************************************************************************
-   * Inserts the  Knolder record into the database                              *
-   **************************************************************************************/
-  def insertKnolder(knolder: Knolder )(implicit session:Session)={
+  /**
+   * ************************************************************************************
+   * Inserts the  knolder record into the database                                      *
+   * ************************************************************************************
+   * @param: knolder record to be deleted
+   */
+  def insertKnolder(knolder: Knolder)(implicit session: Session) = {
     knolTable.insert(knolder)
   }
-    
+  /**
+   * ************************************************************************************
+   * Deletes the  Knolder record from the database table                                *
+   * ************************************************************************************
+   * @param: Id of the knolder
+   */
+  def deleteKnolder(knolderId: Int)(implicit session: Session) = { 
+    knolTableQuery.filter(_.id === knolderId).delete
+  }
+  /**
+   * ************************************************************************************
+   * Updates the  Knolder record from the database table                                *
+   * ************************************************************************************
+   * @param: Knolder Record
+   */
   
+ def updateKnolder(knolder: Knolder)(implicit session: Session) ={
+  val knolList = knolTableQuery.filter {x => x.id === knolder.id }.update(knolder)
+ }
   
+  /**
+   * ************************************************************************************
+   * Returns the  Knolder record by id from the database table                                *
+   * ************************************************************************************
+   * @param: Id of the knolder
+   */
+  
+  def getKnolderById(knolderId:Int)(implicit session: Session):Knolder ={
+    val knolList = knolTableQuery.filter { x => x.id === knolderId }.list
+    knolList.head
+  } 
 } 
 
